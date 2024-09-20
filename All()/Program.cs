bd3da9dc52e18db1e1bd2514bfd0ba17ed7dea75
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-09-20 16:42:39 +0530

    Add files via upload

---
+++ All()/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // 1.Any() with a Condition
+
+            List<int> numbers = new List<int> { 23, 25, 28, 22, 28 };
+
+            bool isAnyNumberGreaterThan10 = numbers.All(number=>number>10);
+            Console.WriteLine(isAnyNumberGreaterThan10);
+
+           
+        }
+    }
+}
